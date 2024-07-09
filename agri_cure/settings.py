9ca9fade271610c6fc8021a05176f01aedbf22ed
Ankit0200 <ankitdevkota107@gmail.com>
2024-07-09 20:16:27 +0545

    something

--- agri_cure/settings.py
+++ agri_cure/settings.py
@@ -157,9 +157,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -178,4 +178,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = BASE_DIR/"staticfiles_build"/"static"
